420836d2ea72222c5c4d62e5531c6155c6257612
your github name <your@git.email>
2022-02-03 07:10:21 +0545

    model created

---
+++ Client/src/App.jsx
@@ -0,0 +1,12 @@
+import "./app.scss"
+
+import Home from "./Home/Home";
+import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+
+const App = () => {
+  return <Home/>;
+};
+
+export default App;
